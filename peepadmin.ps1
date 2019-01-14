3e374e0f305e9e615a9b717e60597d81a9fed097
RH <incredincomp@gmail.com>
2019-01-13 20:29:18 -0800

    Update peepadmin.ps1

--- peepadmin.ps1
+++ peepadmin.ps1
@@ -1,7 +1,3 @@
 #An administrator who has been assigned the Full Access permission to a user's mailbox is considered a delegated user.
 #
 
-$UserMailboxes = Get-mailbox -Filter {(RecipientTypeDetails -eq 'UserMailbox')}
-
-$UserMailboxes | ForEach {Set-Mailbox $_.Identity -AuditEnabled $true}
-
