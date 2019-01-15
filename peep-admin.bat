e3c814776405ed5213a2ddbc31a38eeb2607ced5
RH <incredincomp@gmail.com>
2019-01-14 18:38:38 -0800

    Update peep-admin.bat

--- peep-admin.bat
+++ peep-admin.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%peep-admin.ps1
+SET peep-adminDirectory=%~dp0
+SET peep-adminScriptPath=%peep-adminDirectory%peep-admin.ps1
 
-PowerShell.exe -WindowStyle Hidden -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}"
+PowerShell.exe -WindowStyle Hidden -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File ""%peep-adminScriptPath%""' -Verb RunAs}"
 
 #.\peep-admin.ps1 | .\notify.ps1
