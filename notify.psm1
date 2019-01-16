c57294a5d8a4c9c81c9c466ca96b92127b9f5549
RH <incredincomp@gmail.com>
2019-01-16 12:20:17 -0800

    Update notify.psm1

--- notify.psm1
+++ notify.psm1
@@ -1,10 +1,29 @@
-function newSearch {
+function Schedule 
+{
 
+
+}
+
+function New-Search 
+{
+#Search-AdminAuditLog
+#      [[-Cmdlets <MultiValuedProperty>]
+#      [-DomainController <Fqdn>]
+#      [-EndDate <ExDateTime>]
+#      [-IsSuccess <$true | $false>]
+#      [-ObjectIds <MultiValuedProperty>]
+#      [-Parameters <MultiValuedProperty>]
+#      [-ResultSize <Int32>]
+#      [-StartDate <ExDateTime>]
+#      [-StartIndex <Int32>]
+#      [-UserIds <MultiValuedProperty>]
+#      [-ExternalAccess <$true | $false>]
+#      [<CommonParameters>]
 }
 
 
 
-function notify 
+function Display-Notify 
 {
 #this adds pop up functionality with Add-Type to add the PresentationFramework from Microsoft system files
 Add-Type -AssemblyName PresentationCore,PresentationFramework
@@ -15,6 +34,7 @@ Add-Type -AssemblyName PresentationCore,PresentationFramework
   $MessageIcon = [System.Windows.MessageBoxImage]::Warning #sets the icon for the pop up box to an exclamation warning
   $Result = [System.Windows.MessageBox]::Show($MessageboxBody,$MessageboxTitle,$ButtonType,$MessageIcon) 
   #Saves the message box result to a variable for condition checking
+  [console]::beep(1000,0) #triggers a tone to tag along with notification box
   switch( $Result ) 
   { #conditional statement to decide what is to be done with the information and if the user would like further information
 	  OK     { $ShowMore }
